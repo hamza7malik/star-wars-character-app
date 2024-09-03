544808eb98e2bf43c6b3711d31f029ed1047d42a
Hamza Malik <37263285+hamza7malik@users.noreply.github.com>
2024-09-03 21:55:50 +0400

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className='flex justify-center items-center'>
+      <h1>Star Wars Charatcer App</h1>
+    </div>
+  );
+}

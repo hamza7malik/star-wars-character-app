d70d52645aa3349e44a85a6b84e52e9949eb27c4
Hamza Malik <37263285+hamza7malik@users.noreply.github.com>
2024-09-05 00:53:12 +0400

    character card added

---
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -0,0 +1,47 @@
+import {
+  createGradientString,
+  filterValidColors,
+  truncateText,
+} from '@/src/utils/helpers';
+import Image from 'next/image';
+import React from 'react';
+
+type CharacterCardProps = {
+  id: string;
+  name: string;
+  specieColors: string[];
+  imageUrl: string;
+  onClick: () => void;
+};
+
+const CharacterCard = ({
+  id,
+  name,
+  specieColors,
+  imageUrl,
+  onClick,
+}: CharacterCardProps) => {
+  const colors = specieColors;
+  const validSpecieColors = filterValidColors(colors.join(','));
+
+  const gradientStyles = createGradientString(validSpecieColors);
+  return (
+    <div
+      className={`w-[100%] h-[auto] p-2 flex justify-center items-center rounded-lg`}
+      style={{ background: gradientStyles }}
+      role='button'
+      tabIndex={0}
+    >
+      <div>
+        <Image src={imageUrl} alt={`${name}-image`} width={200} height={250} />
+        <div className='p-1 mt-2 bg-black bg-opacity-70'>
+          <p className='font-semibold text-white pl-1'>
+            {truncateText(name, 20)}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CharacterCard;
